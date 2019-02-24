d0cd3af12ad9264d25d851ab0a1863d9fa3b919d
Scott Dickerson <swiftfoot@hotmail.com>
2019-02-24 11:08:19 -0600

    fix(artistdetails): small style fixes

--- src/containers/RockabillyArtistDetails/__fixtures__/RockabillyArtistDetails.fixture.js
+++ src/containers/RockabillyArtistDetails/__fixtures__/RockabillyArtistDetails.fixture.js
@@ -2,6 +2,7 @@ import RockabillyArtistDetails from "../RockabillyArtistDetails";
 import billyRileySrc from "../../RockabillyCarousel/img/234x234-BillyLeeRiley.png";
 import billyRileyDetails from "../img/616x507-BillyLeeRiley.png";
 import billyRileyAudio from "../../RockabillyAudio/mp3s/01 trouble bound biley lee riley.mp3";
+import bobbyBrownSrc from "../img/616x507-BobbyBrown.png";
 
 const artists = [
   {
@@ -21,13 +22,42 @@ const artists = [
       writtenBy: "Billy Lee Riley",
       performedBy: "Billy Lee Riley"
     }
+  },
+  {
+    id: 1,
+    name: "Billy Lee Riley",
+    description:
+      "In 1933, Billy Lee Riley was born the son of a sharecropper in Pocahontas, where he learned to play the guitar from black farmworkers. He signed with Sun Records in 1956 and released Trouble Bound, with Rock with Me Baby on the B side. Riley followed up with Red Hot in 1958, and while the record had the potential to be a hit, it never found commercial success because Sun chose to focus on promoting Jerry Lee Lewis instead. Riley went on to release several more records, but they also failed to find a widespread audience. He eventually moved to Los Angeles, where he played with the likes of Dean Martin and The Beach Boys. In a twist of fate, Riley experienced newfound popularity in the late 1970s with the Rockabilly Revival in Europe and released new records in the 1990s and 2000s. Riley passed away on August 2, 2009 in Jonesboro. ",
+    thumbnail: billyRileySrc,
+    detailImage: {
+      src: bobbyBrownSrc,
+      description: "Billy Lee Riley, date unknown",
+      photoCredit: "Art Meripol"
+    },
+    audio: {
+      src: billyRileyAudio,
+      songTitle: "Trouble Bound, Sun Records, 1956",
+      writtenBy: "Billy Lee Riley",
+      performedBy: "Billy Lee Riley"
+    }
   }
 ];
 
-export default {
-  component: RockabillyArtistDetails,
-  props: {
-    selectedSite: artists[0],
-    onCloseSite: () => console.log("close the site!")
+export default [
+  {
+    component: RockabillyArtistDetails,
+    name: "vertical image",
+    props: {
+      selectedSite: artists[0],
+      onCloseSite: () => console.log("close the site!")
+    }
+  },
+  {
+    component: RockabillyArtistDetails,
+    name: "horizontal image",
+    props: {
+      selectedSite: artists[1],
+      onCloseSite: () => console.log("close the site!")
+    }
   }
-};
+];
