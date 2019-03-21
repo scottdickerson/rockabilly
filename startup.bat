b13578911a102b336fd001b7dab38f5bbc5737bf
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2019-03-21 09:27:28 -0500

    fix(startup): update startup script

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Chrome Kiosk Mode
 taskkill /f /im explorer.exe
-start "" /AboveNormal "c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk -fullscreen -incognito --disable-pinch --overscroll-history-navigation=0 /max http://localhost:5000
+start "" "c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk --start-fullscreen --incognito --disable-pinch --overscroll-history-navigation=0 /max http://localhost:5000
